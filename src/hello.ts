646116690b31517ea207ec84efbe0c30f645efe9
Herbert Ho <herbert.ho@gmail.com>
2019-08-23 10:01:26 -0700

    auto format with prettier

--- src/hello.ts
+++ src/hello.ts
@@ -1,13 +1,13 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.get('/', function(req:any, res:any) {
-    res.send('hello world!');
+app.get("/", function(req: any, res: any) {
+  res.send("hello world!");
 });
 
 app.listen(3000, function() {
-    console.log('app listening on port 3000');
+  console.log("app listening on port 3000");
 });
